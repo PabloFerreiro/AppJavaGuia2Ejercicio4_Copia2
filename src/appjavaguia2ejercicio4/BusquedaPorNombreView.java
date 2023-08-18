e5060915203381dd70a8dffafacb73cf441eb381
PabloFerreiro <141683969+PabloFerreiro@users.noreply.github.com>
2023-08-18 19:59:36 -0300

    Update BusquedaPorNombreView.java

--- src/appjavaguia2ejercicio4/BusquedaPorNombreView.java
+++ src/appjavaguia2ejercicio4/BusquedaPorNombreView.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-Prueba sobreprueba
+PRUEBA BORRADA EN RAMA 1
 */
 package appjavaguia2ejercicio4;
 
