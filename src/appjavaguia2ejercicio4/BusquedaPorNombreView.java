fe998261992da0e33dd1b94c4fcd8a1329663e90
DavidDanielMoreira <daviddmoreira77@gmail.com>
2023-08-18 19:53:33 -0300

    modifico comentario

--- src/appjavaguia2ejercicio4/BusquedaPorNombreView.java
+++ src/appjavaguia2ejercicio4/BusquedaPorNombreView.java
@@ -2,7 +2,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+Prueba 
+*/
 package appjavaguia2ejercicio4;
 
 import javax.swing.table.DefaultTableModel;
